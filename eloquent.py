d2e1acf761389b04e8920634a3966dc9602242c9
Alexander Schepanovski <suor.web@gmail.com>
2014-05-10 12:49:55 +0800

    More matchers
    - Const, Regex
    - case insensitive keywords and mappings

--- eloquent.py
+++ eloquent.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from funcy import *
 
 
@@ -18,6 +19,8 @@ class Pattern(list):
             p = Pattern(self)
             p.append(other)
             return p
+        elif isinstance(other, (str, unicode)):
+            return self + Const(other)
         else:
             return NotImplemented
 
@@ -29,11 +32,39 @@ class Matcher(object):
     def __add__(self, other):
         if isinstance(other, Matcher):
             return Pattern([self, other])
+        elif isinstance(other, basestring):
+            return Pattern([self, Const(other)])
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, basestring):
+            return Pattern([Const(other), self])
         else:
             return NotImplemented
 
     def to_p(self):
         return Pattern([self])
+
+
+class Const(Matcher):
+    def __init__(self, const):
+        self.const = const
+
+    def __repr__(self):
+        return 'Const(%r)' % self.const
+
+    def match(self, word):
+        if word == self.const:
+            return {}
+
+class Regex(Matcher):
+    def __init__(self, regex):
+        self.regex = re.compile(regex)
+
+    def match(self, word):
+        if self.regex.search(word):
+            return {}
 
 
 class Keyword(Matcher):
@@ -44,6 +75,16 @@ class Keyword(Matcher):
     def match(self, word, context=None):
         if word in self.variants:
             return {self.name: word}
+
+class CIKeyword(Matcher):
+    def __init__(self, name, variants):
+        self.name = name
+        self.mapping = {v.lower(): v for v in variants}
+
+    def match(self, word, context=None):
+        key = word.lower()
+        if key in self.mapping:
+            return {self.name: self.mapping[key]}
 
 
 class Mapping(Matcher):
@@ -56,14 +97,49 @@ class Mapping(Matcher):
             return {self.name: self.mapping[word]}
 
 
+class CIMapping(Matcher):
+    def __init__(self, name, mapping):
+        self.name = name
+        self.mapping = {k.lower(): v for k, v in mapping.items()}
+
+    def match(self, word, context=None):
+        key = word.lower()
+        if key in self.mapping:
+            return {self.name: self.mapping[key]}
+
+
+class Int(Matcher):
+    def __init__(self, name, lower=None, upper=None):
+        self.name = name
+        self.lower = lower or float('-inf')
+        self.upper = upper or float('inf')
+
+    def match(self, word):
+        value = silent(int)(word)
+        if value and self.lower <= value <= self.upper:
+            return {self.name: value}
+
+
+brands = ['Toyota', 'BMW']
+models = {'Corolla': 'Toyota Corolla'}
+
 patterns = [
-    Keyword('brand', ['Toyota', 'BMW']).to_p(),
-    Mapping('model', {'Corolla': 'Toyota Corolla'}).to_p(),
-    # Int('year', 1900, 2014) + Const(u'года'),
+    CIKeyword('brand', brands) + CIMapping('model', models),
+    CIKeyword('brand', brands).to_p(),
+    CIMapping('model', models).to_p(),
+    Regex(u'^(с|от)$') + Int('year__ge', 1900, 2014) + Regex(u'^г(ода?)?$'),
+    Regex(u'^(по|до)$') + Int('year__le', 1900, 2014) + Regex(u'^г(ода?)?$'),
+    Int('year', 1900, 2014) + Regex(u'^г(ода?)?$'),
+    Int('year', 1900, 2014).to_p(),
 ]
 
+import traceback, sys
+
 def parse(s):
+    # print >> sys.stderr, '=' * 100
+    # traceback.print_stack()
     words = s.split()
+    print 'words', words
     return some(p.match(words) for p in patterns)
 
 
--- test_eloquent.py
+++ test_eloquent.py
@@ -20,9 +20,11 @@ def test_keyword():
 def test_orphan_keyword():
     assert parse(u'Corolla') == {'model': 'Toyota Corolla'}
 
-# def test_keyword_case():
-#     assert parse(u'toyota') == {'brand': 'Toyota'}
-#     assert parse(u'toyota corolla') == {'brand': 'Toyota', 'model': 'Toyota Corolla'}
+def test_keyword_case():
+    assert parse(u'toyota') == {'brand': 'Toyota'}
+    assert parse(u'TOYOTA') == {'brand': 'Toyota'}
+    assert parse(u'corolla') == {'model': 'Toyota Corolla'}
+    assert parse(u'toyota corolla') == {'brand': 'Toyota', 'model': 'Toyota Corolla'}
 
 # def test_translit_keyword():
 #     assert parse(u'тойота') == {'brand': 'Toyota'}
@@ -44,16 +46,17 @@ def test_orphan_keyword():
 #     assert False # TODO: add more examples
 
 
-# def test_int_keyword():
-#     assert parse(u'2009 года') == {'year': 2009}
-#     assert parse(u'2010 г') == {'year': 2010}
+def test_int_keyword():
+    assert parse(u'2009 года') == {'year': 2009}
+    assert parse(u'2010 г') == {'year': 2010}
 
-# def test_orphan_int():
-#     assert parse(u'2009') == {'year': 2009}
+def test_orphan_int():
+    assert parse(u'2009') == {'year': 2009}
 
 # def test_range():
 #     assert parse(u'с 2009 по 2012 год') == {'year__range': (2009, 2012)}
 #     assert parse(u'от 2009 до 2012 года') == {'year__range': (2009, 2012)}
+    # assert parse(u'от 2009 до 2012 года') == {'year__range': (2009, 2012)}
 
 # def test_orphan_range():
 #     assert parse(u'от 2009 до 2012') == {'year__range': (2009, 2012)}
@@ -64,11 +67,11 @@ def test_orphan_keyword():
 #     assert parse(u'с 9 по 12') == {'year__range': (2009, 2012)}
 
 
-# def test_bound():
-#     assert parse(u'c 2009 года') == {'year__ge': 2009}
-#     assert parse(u'от 2009 года') == {'year__ge': 2009}
-#     assert parse(u'по 2012 года') == {'year__le': 2012}
-#     assert parse(u'до 2012 года') == {'year__le': 2012}
+def test_bound():
+    assert parse(u'с 2009 года') == {'year__ge': 2009}
+    assert parse(u'от 2009 года') == {'year__ge': 2009}
+    assert parse(u'по 2012 года') == {'year__le': 2012}
+    assert parse(u'до 2012 года') == {'year__le': 2012}
 
 # def test_orphan_bound():
 #     assert parse(u'от 2009') == {'year__ge': 2009}
